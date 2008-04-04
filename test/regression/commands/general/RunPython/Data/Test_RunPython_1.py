9cecf43d7d44be4dec16b60d747902a1208f0dd3
Steve Malers <steve.malers@openwaterfoundation.org>
2008-04-04 19:59:10 +0000

    Save command test folder that has SVN ignore property set for Results.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-test/java_142/trunk@169 fa08ced2-b08f-4ccc-859c-2e92e4a1381a

---
+++ test/regression/commands/general/RunPython/Data/Test_RunPython_1.py
@@ -0,0 +1,12 @@
+#
+# Test command for python running from TSTool
+#
+import sys
+print "start of script"
+ofile = sys.argv[0]
+print "output file is " + ofile
+#f=open('../Results/Test_RunPython_1_out.txt','w')
+#f.writeline("some output")
+#f.close()
+#print "processed the file"
+print "end of script"
