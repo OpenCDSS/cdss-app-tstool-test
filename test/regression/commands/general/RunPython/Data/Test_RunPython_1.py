dcffe4a3eb296cd3c794fa5fc59a74c34ef0c04b
Steve Malers <steve.malers@openwaterfoundation.org>
2008-06-26 15:22:19 +0000

    Update regression test for command.
    git-svn-id: file:///cygdrive/C/owf-svnrepos/TSTool-test/java_142/trunk@194 fa08ced2-b08f-4ccc-859c-2e92e4a1381a

--- test/regression/commands/general/RunPython/Data/Test_RunPython_1.py
+++ test/regression/commands/general/RunPython/Data/Test_RunPython_1.py
@@ -1,12 +1,25 @@
 #
-# Test command for python running from TSTool
+# Test command for running Python script from TSTool
 #
 import sys
+import os
 print "start of script"
-ofile = sys.argv[0]
-print "output file is " + ofile
-#f=open('../Results/Test_RunPython_1_out.txt','w')
-#f.writeline("some output")
-#f.close()
-#print "processed the file"
+print 'os.getcwd()="' + os.getcwd() + '"'
+infile = None
+outfile = None
+if ( len(sys.argv) < 3 ):
+    print "Error.  Expecting input file name as first command line argument, output file name as second."
+    sys.exit(1)
+else:
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    print 'Input file to process is "' + infile + '"'
+    print 'Output file to create is "' + outfile + '"'
+
+inf=open(infile,'r')
+outf=open(outfile,'w')
+for line in inf:
+    outf.write("out: " + line)
+inf.close()
+outf.close()
 print "end of script"
