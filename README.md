fa07ada87197bc6caff36374c59b5f75341b91a0
Steve Malers <steve.malers@openwaterfoundation.org>
2014-12-13 22:26:56 -0700

    Add all changes from TSTool 10.27.00.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # cdss-app-tstool-test #
 
 TSTool functional tests consisting of TSTool command files used in automated regression tests.
+
+Dependencies
+------------
+
+The functional tests are run with TSTool using the operational configuration in the cdss-app-tstool-test/test/operational/CDSS folder.
--- test/regression/commands/general/ReadTableFromDataStore/Data/SqlFile_DivTotal.sql
+++ test/regression/commands/general/ReadTableFromDataStore/Data/SqlFile_DivTotal.sql
@@ -1,4 +1,5 @@
-select * from vw_CDSS_AnnualAmt where meas_num =
+/* Only read specific columns because the HydroBase design changes over time */
+select meas_num, structure_num, quality, irr_year, fdu, ldu, dwc, maxq, maxq_date, nobs, amt_nov, amt_dec from vw_CDSS_AnnualAmt where meas_num =
 (select meas_num from vw_CDSS_StructureStructMeasType
 where wd = 1 and id = 501 and meas_type = 'DivTotal' and time_step='Annual')
 and irr_year >= 1970 and irr_year < 1980 order by irr_year
